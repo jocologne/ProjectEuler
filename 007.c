25a6d3197777a4b79401369b8b7ef806b860c919
João <wsl@DESKTOP-OM4P07T>
2024-10-25 09:06:53 -0300

    commit

---
+++ 007.c
@@ -0,0 +1,38 @@
+
+//Find the 10001 prime number.
+
+#include <stdio.h>
+
+int isprime(int n);
+
+int main(void)
+{
+	int lim = 10001;
+	int count = 0;
+	int num = 1;
+	while (count < lim)
+	{
+		num++;
+		if (isprime(num))
+			count++;
+	}
+	printf("%d", num);
+}
+
+int isprime(int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	if (n % 2 == 0)
+		return (0);
+	unsigned int i = 3;
+	while (i * i <= n)
+	{
+		if ((n % i) == 0 )
+			return (0);
+		i++;
+	}
+	return (1);
+}
