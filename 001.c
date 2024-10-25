a2a64e4390b01f1cda22ff593754e5186dc90d60
João <wsl@DESKTOP-OM4P07T>
2024-10-25 06:03:23 -0300

    P001

---
+++ 001.c
@@ -0,0 +1,20 @@
+
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+#include <stdio.h>
+
+int main (void)
+{
+	unsigned int result;
+	unsigned int n;
+
+	n = 1;
+	result = 0;
+	while (n < 1000)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			result += n;
+		n++;
+	}
+	printf("RESULT=%d", result);
+}
