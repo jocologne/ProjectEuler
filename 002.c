145eab5909a71179b2600a7981d3297bfc9eb5da
João <wsl@DESKTOP-OM4P07T>
2024-10-25 06:24:36 -0300

    commit

---
+++ 002.c
@@ -0,0 +1,24 @@
+
+/*Find the sum of the even-valued terms of  Fibonacci 
+sequence whose values do not exceed four million*/
+
+#include <stdio.h>
+
+int main (void)
+{
+	unsigned int result;
+	unsigned int temp;
+	unsigned int a = 1;
+	unsigned int b = 2;
+
+	result = 2;
+	while ((a + b) < 4000000)
+	{
+		if ((a + b) % 2 == 0)
+			result += (a + b);
+		temp = b;
+		b = (a + b);
+		a = temp; 
+	}
+	printf("RESULT= %i", result);
+}
