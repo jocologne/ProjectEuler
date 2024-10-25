fa4eb51625008ced086b3472f7ebeb2336a36171
João <wsl@DESKTOP-OM4P07T>
2024-10-25 08:23:54 -0300

    commit

---
+++ 005.c
@@ -0,0 +1,29 @@
+
+/*the smallest positive number that is divisible by all of 
+the numbers from 1 to 20*/
+
+#include <stdio.h>
+
+int isprime(int n);
+
+int main (void)
+{
+	long long num = 1;
+	int i = 1;
+	int r = 1;
+	int limite = 20;
+
+	while (r)
+	{
+		printf("%lld\n", num);
+		i = 2;
+		r = 0;
+		while (i <= limite)
+		{
+			if (num % i != 0)
+				r = 1;
+			i++;
+		}
+		num++;
+	}
+}
