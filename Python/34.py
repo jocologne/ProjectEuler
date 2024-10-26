ab83f04b4025feb515b1b80a7faa65e1f2de38b1
root <root@DESKTOP-MJLJHQU>
2024-10-25 23:10:04 -0300

    commit

---
+++ Python/34.py
@@ -0,0 +1,22 @@
+dit = {'1': 1,
+       '2': 2,
+       '3': 6,
+       '4': 24,
+       '5': 120,
+       '6': 720,
+       '7': 5040,
+       '8': 40320,
+       '9': 362880,
+       '0': 1}
+
+arr = []
+for i in range(3, 100001):
+    n = str(i)
+    print(n)
+    s = 0
+    for d in n:
+        s = s + (dit[d])
+    if int(n) == s:
+        arr.append(n)
+
+print(arr)
