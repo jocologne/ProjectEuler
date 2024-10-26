6fb03957aee31471c0c1700ef32ff796050edfbf
root <root@DESKTOP-MJLJHQU>
2024-10-25 21:44:23 -0300

    commit

---
+++ Python/01.py
@@ -0,0 +1,15 @@
+import time
+
+soma=0
+arr=[]
+limite = 1000
+
+for x in range(1,limite):
+    if x % 3 == 0 or x % 5 == 0:
+        arr.append(x)
+
+
+for x in range(0, len(arr)):
+    soma = soma + arr[x]
+
+print(soma)
