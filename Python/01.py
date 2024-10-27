76196b38f16e4eace95b4da33dea399857bb69fe
João <wsl@DESKTOP-OM4P07T>
2024-10-26 21:21:58 -0300

    commit

--- Python/01.py
+++ Python/01.py
@@ -2,16 +2,10 @@
 
 import time
 
-soma=0
-arr=[]
+soma = 0
 limite = 1000
 
 for x in range(1,limite):
     if x % 3 == 0 or x % 5 == 0:
-        arr.append(x)
-
-
-for x in range(0, len(arr)):
-    soma = soma + arr[x]
-
+        soma = soma + x
 print(soma)
