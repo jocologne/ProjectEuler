3f960f29338b901732001105da6ee76bfb6d6421
João <jocolognesi@gmail.com>
2024-11-27 06:29:23 -0300

    commit

--- Python/30.py
+++ Python/30.py
@@ -6,9 +6,9 @@ numeros=[]
 
 def soma_quinta(n):
     soma = 0
-    b = str(n)
-    for i in b:
-        soma += int(i) ** 5
+    numero = str(n)
+    for digito in numero:
+        soma += int(digito) ** 5
     if soma == n:
         numeros.append(n)
 
